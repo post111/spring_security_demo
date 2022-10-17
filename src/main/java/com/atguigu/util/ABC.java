6d3eec63c3f4a9ed14413b9cd7d72ec8535f7c25
彭文杰 <2653945819@qq.com>
2022-10-17 22:20:18 +0800

    新增ABC

---
+++ src/main/java/com/atguigu/util/ABC.java
@@ -0,0 +1,9 @@
+package com.atguigu.util;
+
+public class ABC {
+    public static final String X = "111111111111";
+
+    public static void main(String[] args) {
+        System.out.println(X);
+    }
+}
