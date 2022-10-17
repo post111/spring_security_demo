cbbdf2b5ffad224087becdf8846af456ed7c13fe
彭文杰 <2653945819@qq.com>
2022-10-17 22:27:47 +0800

    修改了ABC第二个代码

--- src/main/java/com/atguigu/util/ABC.java
+++ src/main/java/com/atguigu/util/ABC.java
@@ -1,7 +1,7 @@
 package com.atguigu.util;
 
 public class ABC {
-    public static final String X = "111111111111";
+    public static final String X = "111111111111ev";
 
     public static void main(String[] args) {
         System.out.println(X);
